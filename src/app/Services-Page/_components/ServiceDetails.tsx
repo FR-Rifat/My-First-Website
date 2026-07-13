85e45d4938e2fa219d7efd6b31734b5aeed6397f
FR Rifat <office.frrifat@gmail.com>
2026-07-13 21:33:59 +0600

    3nd

--- src/app/Services-Page/_components/ServiceDetails.tsx
+++ src/app/Services-Page/_components/ServiceDetails.tsx
@@ -12,7 +12,7 @@ export default function ServiceDetails() {
               At Squareup, our design team is passionate about creating stunning,
               user-centric designs that captivate your audience and elevate your
               brand. We believe that great design is not just about aesthetics;
-              it's about creating seamless and intuitive user experiences.
+              it&apos;s about creating seamless and intuitive user experiences.
             </p>
           </div>
 
